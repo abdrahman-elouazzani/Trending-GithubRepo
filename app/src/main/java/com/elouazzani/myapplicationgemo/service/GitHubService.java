ce619bb037b139bb6f1de74af80df9e8853b7cb7
User <abdrahamnelouazzani0@gmail.com>
2021-03-17 17:10:04 +0100

    GitHub Repo Service and client

---
+++ app/src/main/java/com/elouazzani/myapplicationgemo/service/GitHubService.java
@@ -0,0 +1,15 @@
+package com.elouazzani.myapplicationgemo.service;
+
+import com.elouazzani.myapplicationgemo.model.GitHubRepo;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface GitHubService {
+    @GET("search/repositories?q=created:>2017-10-22&sort=stars&order=desc")
+    Observable<List<GitHubRepo>> getStarredRepositories();
+
+}
